c356579f6198ff2d682d7331d866674755b93e87
ofadiman <ofadiman@gmail.com>
2021-05-24 20:27:05 +0200

    fix: migrate ts-jest to babel-jest

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,12 @@ const { defaults } = require('jest-config')
 
 module.exports = {
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
-  preset: 'ts-jest',
   roots: ['.'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/**/?(*.)test.{ts,tsx}'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/']
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.tsx?$': 'babel-jest'
+  }
 }
